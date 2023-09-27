c29e5d8dc4896337e7b43bebc4035243e6a492b0
코딩천재이종만 <jongman@tukorea.ac.kr>
2023-09-27 23:31:21 +0900

    파일추가

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,3 +1,5 @@
+import random
+
 from pico2d import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
@@ -5,7 +7,7 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-
+hand = load_image('hand_arrow.png')
 
 def handle_events():
     global running
@@ -24,6 +26,9 @@ def handle_events():
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+sx, sy = TUK_WIDTH // 2, TUK_HEIGHT // 2
+rx ,ry = random.randint(-500,500), random.randint(-500,500)
+p =0
 frame = 0
 hide_cursor()
 
@@ -33,7 +38,18 @@ while running:
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
-
+    x = (1-p)*sx + p*rx
+    y = (1 - p) * sy + p * ry
+    p += 0.1
+    if (x== rx and y == ry):
+        sx = x
+        sy = y
+        rx = random.randint(-500,500)
+        ry = random.randint(-500, 500)
     handle_events()
 
 close_canvas()
+
+
+
+
